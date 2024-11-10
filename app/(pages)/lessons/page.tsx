f04679668ea65f93b0fe4126238f38d1725e61c0
joe-calih <joecalih254@gmail.com>
2024-11-10 18:21:25 +0300

    Your commit message here

--- app/(pages)/lessons/page.tsx
+++ app/(pages)/lessons/page.tsx
@@ -1,38 +1,11 @@
+"use client"
+
 import { useState } from 'react'
 import Image from 'next/image'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
 import { ArrowRight, Check } from 'lucide-react'
-
-export const metadata = {
-  title: 'Lessons',
-  description: 'Get in touch with Your Company Name for inquiries, support, and feedback. We are here to assist you.',
-  keywords: ['Contact', 'Support', 'Customer Service', 'Your Company Name'],
-  openGraph: {
-    title: 'Lessons',
-    description: 'Reach out to us for inquiries or support. Contact Your Company Name today!',
-    url: 'https://yourwebsite.com/contact-us',
-    images: [
-      {
-        url: 'https://yourwebsite.com/images/contact-us-banner.jpg',
-        width: 800,
-        height: 600,
-        alt: 'Lessons Banner',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Lessons',
-    description: 'Have any questions? Contact Your Company Name for support and inquiries.',
-    images: ['https://yourwebsite.com/images/contact-us-banner.jpg'],
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
 
 
 const tabContent = {
--- app/(pages)/services/page.tsx
+++ app/(pages)/services/page.tsx
@@ -35,36 +35,6 @@ function YouTubeVideo({ videoId }: { videoId: string }) {
   )
 }
 
-
-export const metadata = {
-  title: 'Services',
-  description: 'Get in touch with Your Company Name for inquiries, support, and feedback. We are here to assist you.',
-  keywords: ['Contact', 'Support', 'Customer Service', 'Your Company Name'],
-  openGraph: {
-    title: 'Services',
-    description: 'Reach out to us for inquiries or support. Contact Your Company Name today!',
-    url: 'https://yourwebsite.com/contact-us',
-    images: [
-      {
-        url: 'https://yourwebsite.com/images/contact-us-banner.jpg',
-        width: 800,
-        height: 600,
-        alt: 'Services Banner',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'Services',
-    description: 'Have any questions? Contact Your Company Name for support and inquiries.',
-    images: ['https://yourwebsite.com/images/contact-us-banner.jpg'],
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
-
 export default function SmartHomeHero() {
   return (
     <div className="w-full bg-background">
